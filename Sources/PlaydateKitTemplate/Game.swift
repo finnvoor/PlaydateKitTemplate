abe255db5b1f68a76affad2cf2341df26de324a5
Finn Voorhees <finnvoorhees@gmail.com>
2024-05-02 19:38:48 +0100

    Subclass Sprite

--- Sources/PlaydateKitTemplate/Game.swift
+++ Sources/PlaydateKitTemplate/Game.swift
@@ -1,25 +1,43 @@
 import PlaydateKit
 
+// MARK: - Game
+
 final class Game: PlaydateGame {
     // MARK: Lifecycle
 
     init() {
-        logoSprite.image = try! Graphics.Bitmap(path: "logo.png")
-        logoSprite.bounds = .init(x: 0, y: 0, width: 400, height: 240)
-        logoSprite.addToDisplayList()
+        logo.addToDisplayList()
     }
 
     // MARK: Internal
 
-    let logoSprite = Sprite.Sprite()
+    let logo = Logo()
 
     func update() -> Bool {
-        Sprite.drawDisplayListSprites()
+        Sprite.updateAndDrawDisplayListSprites()
         System.drawFPS()
         return true
     }
 
     func gameWillPause() {
         System.log("Paused!")
+    }
+}
+
+// MARK: - Logo
+
+class Logo: Sprite.Sprite {
+    // MARK: Lifecycle
+
+    override init() {
+        super.init()
+        image = try! Graphics.Bitmap(path: "logo.png")
+        bounds = .init(x: 0, y: 0, width: 400, height: 240)
+    }
+
+    // MARK: Internal
+
+    override func update() {
+        moveBy(dx: 0, dy: sinf(System.elapsedTime * 4))
     }
 }
