fe483afa36d90cce24f0ee3bef405294c26e5302
STREGA <STREGA@users.noreply.github.com>
2024-11-22 08:41:11 -0500

    Use Swift 6

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 6.0
 
 import Foundation
 import PackageDescription
@@ -19,6 +19,8 @@ let package = Package(
             ],
             swiftSettings: swiftSettings
         ),
+    ],
+    swiftLanguageModes: [.v6]
 )
 
 
