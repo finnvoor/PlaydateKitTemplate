da4546e3345e78c5d40850efc1442d82ed6f911f
STREGA <STREGA@users.noreply.github.com>
2024-11-22 08:42:20 -0500

    Use the product flag added in #104

--- .swiftpm/build-and-run.sh
+++ .swiftpm/build-and-run.sh
@@ -2,5 +2,5 @@
 set -e
 (killall 'Playdate Simulator' || true) 2>/dev/null
 cd ..
-swift package pdc
 ~/Developer/PlaydateSDK/bin/Playdate\ Simulator.app/Contents/MacOS/Playdate\ Simulator .build/plugins/PDCPlugin/outputs/$PRODUCT_NAME.pdx
+swift package pdc --product $PRODUCT_NAME
