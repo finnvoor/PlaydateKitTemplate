a3997ab4c865960129cef06f3bbbe1105b08946d
STREGA <STREGA@users.noreply.github.com>
2024-11-22 08:42:52 -0500

    Create a symbolic link in Playdate Simulator

--- .swiftpm/build-and-run.sh
+++ .swiftpm/build-and-run.sh
@@ -4,3 +4,12 @@ set -e
 cd ..
 ~/Developer/PlaydateSDK/bin/Playdate\ Simulator.app/Contents/MacOS/Playdate\ Simulator .build/plugins/PDCPlugin/outputs/$PRODUCT_NAME.pdx
 swift package pdc --product $PRODUCT_NAME
+
+# Create a symbolic link to the compiled pdx in the Playdate Simulator Games dir
+# This allows browsing the pdx as a sideloaded game, helpful for checking icons
+if [ -e ~/Developer/PlaydateSDK/Disk/Games/$PRODUCT_NAME.pdx ]
+then
+    rm -rf ~/Developer/PlaydateSDK/Disk/Games/$PRODUCT_NAME.pdx
+fi
+ln -s "$(pwd)/.build/plugins/PDCPlugin/outputs/$PRODUCT_NAME.pdx" ~/Developer/PlaydateSDK/Disk/Games/$PRODUCT_NAME.pdx
+
