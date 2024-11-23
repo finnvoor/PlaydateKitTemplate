de73d7e0ce71743234bf3ca0bc3071abdff1e138
Dustin Collins <STREGA@users.noreply.github.com>
2024-11-23 16:22:36 -0500

    Swift 6, Fixes, and Quality of life improvements (#3)
    * Make helper vars computed
    
    * Use Swift 6
    
    * Use the product flag added in #104
    
    * Create a symbolic link in Playdate Simulator
    
    * Use symlink to open the pdx

--- .swiftpm/build-and-run.sh
+++ .swiftpm/build-and-run.sh
@@ -2,5 +2,15 @@
 set -e
 (killall 'Playdate Simulator' || true) 2>/dev/null
 cd ..
-swift package pdc
-~/Developer/PlaydateSDK/bin/Playdate\ Simulator.app/Contents/MacOS/Playdate\ Simulator .build/plugins/PDCPlugin/outputs/$PRODUCT_NAME.pdx
+swift package pdc --product $PRODUCT_NAME
+
+# Create a symbolic link to the compiled pdx in the Playdate Simulator Games dir
+# This allows browsing the pdx as a sideloaded game, helpful for checking icons
+if [ -e ~/Developer/PlaydateSDK/Disk/Games/$PRODUCT_NAME.pdx ]
+then
+    rm -rf ~/Developer/PlaydateSDK/Disk/Games/$PRODUCT_NAME.pdx
+fi
+ln -s "$(pwd)/.build/plugins/PDCPlugin/outputs/$PRODUCT_NAME.pdx" ~/Developer/PlaydateSDK/Disk/Games/$PRODUCT_NAME.pdx
+
+# Run the pdx in the Playdate Simulator
+~/Developer/PlaydateSDK/bin/Playdate\ Simulator.app/Contents/MacOS/Playdate\ Simulator ~/Developer/PlaydateSDK/Disk/Games/$PRODUCT_NAME.pdx
--- Package.swift
+++ Package.swift
@@ -1,17 +1,8 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 6.0
 
 import Foundation
 import PackageDescription
 
-let gccIncludePrefix =
-    "/usr/local/playdate/gcc-arm-none-eabi-9-2019-q4-major/lib/gcc/arm-none-eabi/9.2.1"
-
-let playdateSDKPath: String
-if let path = Context.environment["PLAYDATE_SDK_PATH"] {
-    playdateSDKPath = path
-} else {
-    playdateSDKPath = "\(Context.environment["HOME"]!)/Developer/PlaydateSDK/"
-}
 
 let package = Package(
     name: "PlaydateKitTemplate",
@@ -23,21 +14,40 @@ let package = Package(
     targets: [
         .target(
             name: "PlaydateKitTemplate",
-            dependencies: [.product(name: "PlaydateKit", package: "PlaydateKit")],
-            swiftSettings: [
-                .enableExperimentalFeature("Embedded"),
-                .unsafeFlags([
-                    "-Xfrontend", "-disable-objc-interop",
-                    "-Xfrontend", "-disable-stack-protector",
-                    "-Xfrontend", "-function-sections",
-                    "-Xfrontend", "-gline-tables-only",
-                    "-Xcc", "-DTARGET_EXTENSION",
-                    "-Xcc", "-I", "-Xcc", "\(gccIncludePrefix)/include",
-                    "-Xcc", "-I", "-Xcc", "\(gccIncludePrefix)/include-fixed",
-                    "-Xcc", "-I", "-Xcc", "\(gccIncludePrefix)/../../../../arm-none-eabi/include",
-                    "-I", "\(playdateSDKPath)/C_API"
-                ]),
-            ]
-        )
-    ]
+            dependencies: [
+                .product(name: "PlaydateKit", package: "PlaydateKit")
+            ],
+            swiftSettings: swiftSettings
+        ),
+    ],
+    swiftLanguageModes: [.v6]
 )
+
+
+// MARK: - Helper Variables
+// note: These must be computed variables when beneath the `let package =` declaration.
+
+var swiftSettings: [SwiftSetting] {[
+    .enableExperimentalFeature("Embedded"),
+    .unsafeFlags([
+        "-whole-module-optimization",
+        "-Xfrontend", "-disable-objc-interop",
+        "-Xfrontend", "-disable-stack-protector",
+        "-Xfrontend", "-function-sections",
+        "-Xfrontend", "-gline-tables-only",
+        "-Xcc", "-DTARGET_EXTENSION",
+        "-Xcc", "-I", "-Xcc", "\(gccIncludePrefix)/include",
+        "-Xcc", "-I", "-Xcc", "\(gccIncludePrefix)/include-fixed",
+        "-Xcc", "-I", "-Xcc", "\(gccIncludePrefix)/../../../../arm-none-eabi/include",
+        "-I", "\(playdateSDKPath)/C_API"
+    ]),
+]}
+var gccIncludePrefix: String {
+    "/usr/local/playdate/gcc-arm-none-eabi-9-2019-q4-major/lib/gcc/arm-none-eabi/9.2.1"
+}
+var playdateSDKPath: String {
+    if let path = Context.environment["PLAYDATE_SDK_PATH"] {
+        return path
+    }
+    return "\(Context.environment["HOME"]!)/Developer/PlaydateSDK/"
+}
