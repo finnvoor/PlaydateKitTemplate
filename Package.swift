d1e281d6c981d8617b4a13a29340818e1493400d
Finn Voorhees <finnvoorhees@gmail.com>
2024-12-25 18:44:25 -0500

    Attempt to fix https://github.com/finnvoor/PlaydateKit/discussions/110

--- Package.swift
+++ Package.swift
@@ -17,37 +17,22 @@ let package = Package(
             dependencies: [
                 .product(name: "PlaydateKit", package: "PlaydateKit")
             ],
-            swiftSettings: swiftSettings
+            swiftSettings: [
+                .enableExperimentalFeature("Embedded"),
+                .unsafeFlags([
+                    "-whole-module-optimization",
+                    "-Xfrontend", "-disable-objc-interop",
+                    "-Xfrontend", "-disable-stack-protector",
+                    "-Xfrontend", "-function-sections",
+                    "-Xfrontend", "-gline-tables-only",
+                    "-Xcc", "-DTARGET_EXTENSION",
+                    "-Xcc", "-I", "-Xcc", "/usr/local/playdate/gcc-arm-none-eabi-9-2019-q4-major/lib/gcc/arm-none-eabi/9.2.1/include",
+                    "-Xcc", "-I", "-Xcc", "/usr/local/playdate/gcc-arm-none-eabi-9-2019-q4-major/lib/gcc/arm-none-eabi/9.2.1/include-fixed",
+                    "-Xcc", "-I", "-Xcc", "/usr/local/playdate/gcc-arm-none-eabi-9-2019-q4-major/lib/gcc/arm-none-eabi/9.2.1/../../../../arm-none-eabi/include",
+                    "-I", "\(Context.environment["PLAYDATE_SDK_PATH"] ?? "\(Context.environment["HOME"]!)/Developer/PlaydateSDK/")/C_API"
+                ]),
+            ]
         ),
     ],
     swiftLanguageModes: [.v6]
 )
-
-
-// MARK: - Helper Variables
-// note: These must be computed variables when beneath the `let package =` declaration.
-
-var swiftSettings: [SwiftSetting] {[
-    .enableExperimentalFeature("Embedded"),
-    .unsafeFlags([
-        "-whole-module-optimization",
-        "-Xfrontend", "-disable-objc-interop",
-        "-Xfrontend", "-disable-stack-protector",
-        "-Xfrontend", "-function-sections",
-        "-Xfrontend", "-gline-tables-only",
-        "-Xcc", "-DTARGET_EXTENSION",
-        "-Xcc", "-I", "-Xcc", "\(gccIncludePrefix)/include",
-        "-Xcc", "-I", "-Xcc", "\(gccIncludePrefix)/include-fixed",
-        "-Xcc", "-I", "-Xcc", "\(gccIncludePrefix)/../../../../arm-none-eabi/include",
-        "-I", "\(playdateSDKPath)/C_API"
-    ]),
-]}
-var gccIncludePrefix: String {
-    "/usr/local/playdate/gcc-arm-none-eabi-9-2019-q4-major/lib/gcc/arm-none-eabi/9.2.1"
-}
-var playdateSDKPath: String {
-    if let path = Context.environment["PLAYDATE_SDK_PATH"] {
-        return path
-    }
-    return "\(Context.environment["HOME"]!)/Developer/PlaydateSDK/"
-}
