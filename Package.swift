9b7b32f14b72ccf3fbeb3c10bf5abb611d5632d6
Finn Voorhees <finnvoorhees@gmail.com>
2025-05-30 13:56:25 +0100

    Build using swiftpm (#4)

--- Package.swift
+++ Package.swift
@@ -1,22 +1,25 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.1
 
 import Foundation
 import PackageDescription
 
+/// Hack to force Xcode builds to not produce a dylib, since linking fails
+/// without a toolset.json specified. Ideally this can be removed if/when
+/// Xcode gains toolset.json support.
+let xcode = (Context.environment["XPC_SERVICE_NAME"]?.count ?? 0) > 2
 
 let package = Package(
     name: "PlaydateKitTemplate",
     platforms: [.macOS(.v14)],
-    products: [.library(name: "PlaydateKitTemplate", targets: ["PlaydateKitTemplate"])],
+    products: [.library(name: "PlaydateKitTemplate", type: xcode ? nil : .dynamic, targets: ["PlaydateKitTemplate"])],
     dependencies: [
         .package(url: "https://github.com/finnvoor/PlaydateKit.git", branch: "main"),
     ],
     targets: [
         .target(
             name: "PlaydateKitTemplate",
-            dependencies: [
-                .product(name: "PlaydateKit", package: "PlaydateKit")
-            ],
+            dependencies: [.product(name: "PlaydateKit", package: "PlaydateKit")],
+            exclude: ["Resources"],
             swiftSettings: [
                 .enableExperimentalFeature("Embedded"),
                 .unsafeFlags([
@@ -25,14 +28,9 @@ let package = Package(
                     "-Xfrontend", "-disable-stack-protector",
                     "-Xfrontend", "-function-sections",
                     "-Xfrontend", "-gline-tables-only",
-                    "-Xcc", "-DTARGET_EXTENSION",
-                    "-Xcc", "-I", "-Xcc", "/usr/local/playdate/gcc-arm-none-eabi-9-2019-q4-major/lib/gcc/arm-none-eabi/9.2.1/include",
-                    "-Xcc", "-I", "-Xcc", "/usr/local/playdate/gcc-arm-none-eabi-9-2019-q4-major/lib/gcc/arm-none-eabi/9.2.1/include-fixed",
-                    "-Xcc", "-I", "-Xcc", "/usr/local/playdate/gcc-arm-none-eabi-9-2019-q4-major/lib/gcc/arm-none-eabi/9.2.1/../../../../arm-none-eabi/include",
-                    "-I", "\(Context.environment["PLAYDATE_SDK_PATH"] ?? "\(Context.environment["HOME"]!)/Developer/PlaydateSDK/")/C_API"
+                    "-Xcc", "-DTARGET_EXTENSION"
                 ]),
-            ]
-        ),
-    ],
-    swiftLanguageModes: [.v6]
+            ],
+        )
+    ]
 )
